f0f101825a80723f49498a674c883429a485e844
miguel <gannasong@gmail.com>
2023-01-29 23:27:34 +0800

    extract function to amountFor(_, _)

--- chapter_1.playground/Contents.swift
+++ chapter_1.playground/Contents.swift
@@ -11,24 +11,7 @@ func statement(invoice: Invoice, plays: [String: Play]) -> String {
             fatalError("Not to found play.")
         }
 
-        var thisAmount = 0
-        switch play.type {
-            case "tragedy":
-                thisAmount = 40000
-                if perf.audience > 30 {
-                    thisAmount += 1000 * (perf.audience - 30)
-                }
-                break
-            case "comedy":
-                thisAmount = 30000
-                if perf.audience > 20 {
-                    thisAmount += 10000 + 500 * (perf.audience - 20)
-                }
-                thisAmount += 300 * perf.audience
-                break
-            default:
-                fatalError("unknown type: \(play.type)")
-        }
+        var thisAmount = amountFor(perf, play)
 
         // add volume credits
         volumeCredits += max(perf.audience - 30, 0)
@@ -47,6 +30,32 @@ func statement(invoice: Invoice, plays: [String: Play]) -> String {
     return result
 }
 
+// 106
+func amountFor(_ perf: Performance, _ play: Play) -> Int {
+    var thisAmount = 0
+
+    switch play.type {
+        case "tragedy":
+            thisAmount = 40000
+            if perf.audience > 30 {
+                thisAmount += 1000 * (perf.audience - 30)
+            }
+            break
+        case "comedy":
+            thisAmount = 30000
+            if perf.audience > 20 {
+                thisAmount += 10000 + 500 * (perf.audience - 20)
+            }
+            thisAmount += 300 * perf.audience
+            break
+        default:
+            fatalError("unknown type: \(play.type)")
+    }
+
+    return thisAmount
+}
+
+// Test
 class StatementTests: XCTestCase {
     func test_validate_statementResultEqualToAnyResult() {
         let anyInvoices = makeAnyInvoices()
@@ -92,6 +101,3 @@ class StatementTests: XCTestCase {
 let testObserver = TestObserver()
 XCTestObservationCenter.shared.addTestObserver(testObserver)
 StatementTests.defaultTestSuite.run()
-
-
-
